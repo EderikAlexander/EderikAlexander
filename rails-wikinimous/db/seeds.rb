a67dad18530e661a09549071191fabbd4f41755c
Ederik van Koldenhoven <e.vankoldenhoven@gmail.com>
2017-06-17 13:39:44 +0200

    cleaning up master

--- rails-wikinimous/db/seeds.rb
+++ rails-wikinimous/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  10.times do
+  title_fake = Faker::Lorem.word
+  content_fake = Faker::Lorem.paragraph(2)
+
+  new_article = Article.create(title: title_fake, content: content_fake)
+  end
